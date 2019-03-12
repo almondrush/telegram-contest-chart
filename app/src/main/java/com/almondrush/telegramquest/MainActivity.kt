94ec31e313dcd40c3f9c2a2ea639cc10186b9fa0
Alexander Sirota <personal.alexgator@gmail.com>
2019-03-12 06:10:59 +0300

    Changing time range

--- app/src/main/java/com/almondrush/telegramquest/MainActivity.kt
+++ app/src/main/java/com/almondrush/telegramquest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.almondrush.telegramquest
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +9,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chartView: TelegramChartView = findViewById(R.id.chart_view)
+
+        val seekBar1: SeekBar = findViewById(R.id.seek_bar1)
+        val seekBar2: SeekBar = findViewById(R.id.seek_bar2)
+
+        fun onSeekChanged() {
+            chartView.setTimeRange((seekBar1.progress / 2)..(50 + seekBar2.progress / 2))
+        }
+
+        seekBar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                onSeekChanged()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                onSeekChanged()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 
 }
--- app/src/main/java/com/almondrush/telegramquest/TelegramChartView.kt
+++ app/src/main/java/com/almondrush/telegramquest/TelegramChartView.kt
@@ -1,7 +1,12 @@
 package com.almondrush.telegramquest
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.PointF
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
@@ -13,39 +18,74 @@ class TelegramChartView @JvmOverloads constructor(
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+    companion object {
+        const val TIME_RANGE_MIN = 0
+        const val TIME_RANGE_MAX = 100
+    }
+
     private val paint = Paint()
     private val path = Path()
+    private val pathRect: Rect = Rect()
 
-    private val data = testData
-    private val timeRange: IntRange = 0 until 100
+    private var lineWidth: Float = 5F
 
-    private var pathRect: Rect = Rect()
+    private var shouldUpdate: Boolean = true
+
+    private var timeRange: IntRange = TIME_RANGE_MIN..TIME_RANGE_MAX
+
+    private var lines: List<Line> = emptyList()
+    private var linesPx: List<LinePx> = emptyList()
 
     init {
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = 5F
+        initPaint()
+        setChartData(testData)
+    }
+
+    fun setChartData(data: ChartData) {
+        lines = data.lines.map { line ->
+            Line(Color.parseColor(line.color), line.data.mapIndexed { index, value ->
+                PointL(data.timeData[index], value)
+            })
+        }
+        shouldUpdate = true
+        invalidate()
+    }
+
+    fun setTimeRange(range: IntRange) {
+        require(TIME_RANGE_MIN <= range.first && range.last <= TIME_RANGE_MAX)
+        timeRange = range
+        shouldUpdate = true
+        invalidate()
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         pathRect.set(0, measuredHeight, measuredWidth, 0)
+        // Stroke width padding
+        val strokePadding = (lineWidth / 2).toInt()
+        pathRect.set(
+            pathRect.left + strokePadding,
+            pathRect.top - strokePadding,
+            pathRect.right - strokePadding,
+            pathRect.bottom + strokePadding
+        )
+        shouldUpdate = true
     }
 
     override fun onDraw(canvas: Canvas) {
         measureTimeMillis {
-            d("pathRect: $pathRect")
+            if (shouldUpdate) {
+                linesPx = updatePixelValues(lines, timeRange, pathRect)
+                shouldUpdate = false
+            }
 
-            val xValues = timeDataToPixelValues(data.timeData, pathRect)
-            val yValuesList = linesDataToPixelValues(data.lines, pathRect)
-
-            d(xValues.toList().toString())
-
-            yValuesList.forEachIndexed { lineIndex, yValues ->
+            linesPx.forEach { line ->
                 path.rewind()
-                paint.color = Color.parseColor(data.lines[lineIndex].color)
-                path.moveTo(xValues[0], yValues[0])
-                for (i in 1 until xValues.size) path.lineTo(xValues[i], yValues[i])
-                path.moveTo(xValues[0], yValues[0])
+                paint.color = line.color
+                line.data.forEachIndexed { index, point ->
+                    if (index == 0) path.moveTo(point.x, point.y) else path.lineTo(point.x, point.y)
+                }
                 canvas.drawPath(path, paint)
             }
 
@@ -54,45 +94,72 @@ class TelegramChartView @JvmOverloads constructor(
         }
     }
 
-    private fun timeDataToPixelValues(timeData: Array<Long>, pathRect: Rect): Array<Float> {
-        val startXPixel = pathRect.left
-        val endXPixel = pathRect.right
-        val xPixelCount = endXPixel - startXPixel
-        val pixelsPerTime = xPixelCount.toFloat() / (timeData.last() - timeData.first()).toFloat()
-
-        return Array(timeData.size) { i ->
-            (timeData[i] - timeData[0]) * pixelsPerTime
+    private fun initPaint() {
+        paint.apply {
+            style = Paint.Style.STROKE
+            strokeWidth = lineWidth
+            isAntiAlias = true
         }
     }
 
-    private fun linesDataToPixelValues(lines: List<ChartLine>, pathRect: Rect): List<Array<Float>> {
-        val startYPixel = pathRect.bottom
-        val endYPixel = pathRect.top
-        val yPixelCount = endYPixel - startYPixel
-
-        val (minYValue, maxYValue) = findMinAndMaxValues(lines)
-
-        val pixelsPerValue = yPixelCount.toFloat() / (maxYValue - minYValue).toFloat()
-
+    private fun updatePixelValues(lines: List<Line>, timeRange: IntRange, pathRect: Rect): List<LinePx> {
+        val (minY, maxY) = findMinAndMaxYValues(lines)
         return lines.map { line ->
-            Array(line.data.size) { i ->
-                pathRect.top - ((line.data[i] - minYValue) * pixelsPerValue)
+            val rangedValues = fitValuesIntoRange(line.data, timeRange)
+            LinePx(line.color, convertToPixelValues(rangedValues, pathRect, minY, maxY))
+        }
+    }
+
+    private fun fitValuesIntoRange(values: List<PointL>, selectedRange: IntRange): List<PointL> {
+        val timeInterval = values.last().x - values.first().x
+
+        val startTime = values.first().x + timeInterval * selectedRange.start / TIME_RANGE_MAX
+        val endTime = values.last().x - timeInterval * (TIME_RANGE_MAX - selectedRange.endInclusive) / TIME_RANGE_MAX
+
+        var rangedValues = values.filter { it.x in startTime..endTime }
+
+        val firstRangedValue = rangedValues.first()
+        val lastRangedValue = rangedValues.last()
+
+        //        val firstRangedValueIndex = values.indexOf(firstRangedValue)
+        //        val lastRangedValueIndex = values.indexOf(lastRangedValue)
+        //TODO add synthetic points
+        return rangedValues
+    }
+
+    private fun convertToPixelValues(
+        values: List<PointL>,
+        pathRect: Rect,
+        minYValue: Long,
+        maxYValue: Long
+    ): Array<PointF> {
+        val pixelsPerXValue = (pathRect.right - pathRect.left).toFloat() / (values.last().x - values.first().x)
+        val pixelsPerYValue = (pathRect.top - pathRect.bottom).toFloat() / (maxYValue - minYValue)
+
+        return Array(values.size) { i ->
+            val currentValue = values[i]
+            val time = (currentValue.x - values.first().x) * pixelsPerXValue
+            val value = pathRect.top - ((currentValue.y - minYValue) * pixelsPerYValue)
+            PointF(time, value)
+        }
+    }
+
+    private fun findMinAndMaxYValues(lines: List<Line>): Pair<Long, Long> {
+        var maxY = lines.first().data.first().y
+        var minY = lines.first().data.first().y
+        lines.forEach { line ->
+            line.data.forEach {
+                if (it.y > maxY) maxY = it.y
+                if (it.y < minY) minY = it.y
             }
         }
-    }
-
-    private fun findMinAndMaxValues(lines: List<ChartLine>): Pair<Long, Long> = lines.map { line ->
-        var lineMin = line.data.first()
-        var lineMax = line.data.first()
-        line.data.forEach {
-            if (it < lineMin) lineMin = it
-            if (it > lineMax) lineMax = it
-        }
-        lineMin to lineMax
-    }.reduce { (oldMin, oldMax), (currentMin, currentMax) ->
-        Math.min(oldMin, currentMin) to Math.max(oldMax, currentMax)
+        return minY to maxY
     }
 
     fun d(t: String) =  Log.d("CHART_VIEW", t)
+
+    data class Line(val color: Int, val data: List<PointL>)
+    data class LinePx(val color: Int, val data: Array<PointF>)
+    data class PointL(val x: Long, val y: Long)
 
 }
